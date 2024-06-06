22a410422f1d0bff77274d9d8117f5056d01c017
fadhluibnu <f.ibnu.a1@gmail.com>
2024-06-07 00:17:30 +0700

    refactor: restructure modular code karateristik

--- pages/sikd/ui_karakteristik.R
+++ pages/sikd/ui_karakteristik.R
@@ -1,67 +1,48 @@
-library(shiny)
-library(shinydashboard)
-library(readr)
-library(DT)
-library(ggplot2)
-library(dplyr)
-library(viridis) 
-
-
-ui_karakteristik <- function(id) {
-  ns <- NS(id)
-    tabItem(tabName = "karakteristik",
-            fluidRow(
-              box(title = "Distribusi Usia", width = 6, 
-                  plotOutput("age_bar_chart")),
-              box(title = "Jenis Kelamin", width = 6, 
-                  plotOutput("pie_chart"))
-            ),
-            fluidRow(
-              box(title = "Karakteristik Desa", width = 12, 
-                  DTOutput("data_table"))
-            )
-    )
-}
-
-server_karakteristik <- function(input, output, session, data) {
-  ns <- session$ns
-  
-  print("Server Karakteristik called")
-  print(str(data))
-  
-  output[[ns("data_table")]] <- renderDT({
-    datatable(data)
-  })
-  
-  # Render pie chart
-  output[[ns("pie_chart")]] <- renderPlot({
-    gender_count <- data %>%
-      count(jenis_kelamin)
-    
-    ggplot(gender_count, aes(x = "", y = n, fill = jenis_kelamin)) +
-      geom_bar(stat = "identity", width = 1) +
-      coord_polar("y") +
-      theme_void() +
-      labs(fill = "Jenis Kelamin", title = "Distribusi Jenis Kelamin")
-  })
-  
-  # Render bar chart for age distribution
-  output[[ns("age_bar_chart")]] <- renderPlot({
-    age_count <- data %>%
-      count(Usia)
-    
-    # Define a custom color palette
-    custom_colors <- c("red", "green", "blue", "orange", "purple", "yellow", "pink", "brown", "cyan", "magenta")
-    # Shuffle the colors
-    set.seed(42) # For reproducibility
-    shuffled_colors <- sample(custom_colors, length(unique(age_count$Usia)), replace = TRUE)
-    
-    ggplot(age_count, aes(x = factor(Usia), y = n, fill = factor(Usia))) + 
-      geom_bar(stat = "identity") +
-      scale_fill_manual(values = shuffled_colors) + # Use custom shuffled colors
-      scale_y_continuous(breaks = seq(0, max(age_count$n), by = 1)) + # Ensure integer breaks
-      theme_minimal() +
-      labs(x = "Usia", y = "Jumlah", title = "Distribusi Usia") +
-      theme(legend.position = "none") # Remove legend
-  })
-}
+tabItemKarakteristik <- tabItem(tabName = "karakteristik",
+                                fluidRow(
+                                  conditionalPanel(
+                                    condition = "input.jenis_kelamin == true",
+                                    box(title = "Jenis Kelamin", width = 6, 
+                                        plotOutput("pie_chart_jenis_kelamin"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.pendidikan == true",
+                                    box(title = "Pendidikan", width = 6,
+                                        plotOutput("pie_chart_pendidikan"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.show_age_chart == true",
+                                    box(id = "age_bar_chart_box", title = "Distribusi Usia", width = 12, 
+                                        plotOutput("age_bar_chart"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.pekerjaan_utama == true",
+                                    box(title = "Pekerjaan Utama", width = 12, 
+                                        plotOutput("bar_chart_pekerjaan_utama"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.pekerjaan_sampingan == true",
+                                    box(title = "Pekerjaan Sampingan", width = 6, 
+                                        plotOutput("bar_chart_pekerjaan_sampingan"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.memulai_usaha == true",
+                                    box(title = "Memulai Usaha", width = 6, 
+                                        plotOutput("bar_chart_memulai_usaha"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.jenis_usaha == true",
+                                    box(title = "Jenis Usaha", width = 6, 
+                                        plotOutput("bar_chart_jenis_usaha"))
+                                  ),
+                                  conditionalPanel(
+                                    condition = "input.skala_usaha == true",
+                                    box(title = "Skala Usaha", width = 6, 
+                                        plotOutput("pie_chart_skala_usaha"))
+                                  ),
+                                ),
+                                fluidRow(
+                                  box(title = "Karakteristik Desa", width = 12, 
+                                      DTOutput("data_table"))
+                                )
+)
